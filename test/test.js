33fc2aeadc8517435b25eed393bfefd1f0610e62
Kieran <kieran@nitrexdesign.co.uk>
2023-04-30 03:51:10 +0100

    Add test to build docker images (#5)
    * Add test to verify docker images build.
    
    * chore: run eslint
    
    * try including docker/setup-buildx-action to enable gh actions to build docker images
    
    * update Dockerfiles in `test`
    
    * add test-docker temporary actions job
    
    * update Dockerfile template, syntax v1.4 required for `COPY --link`
    
    * use `docker buildx` to build images, added environment flag for running docker build test
    
    * cleanup dev helpers
    
    * fix stupid mistake
    
    * fix issue with docker build on actions
    
    * fix failure of test suite because of an exception in a step
    
    * remove console log
    
    * undo machine-specific changes
    
    * extend timeout to 5 minutes for with_docker test
    
    * add cleanup step to tests, possible fix to exit code 5
    
    * changed from deleting images to pruning the build cache
    
    * undo cleanup work
    
    * add just enough of an application to build
    
    * Update test/remix-indie/app/root.tsx
    
    * get tests passing again
    
    * test `--parallel` flag performance on actions
    
    * test limits of parallel speedup
    
    * revert to serial
    
    ---------
    
    Co-authored-by: Sam Ruby <rubys@intertwingly.net>
    Co-authored-by: Michaël De Boey <info@michaeldeboey.be>

--- test/test.js
+++ test/test.js
@@ -5,64 +5,85 @@ import fs from 'node:fs'
 import { expect } from 'chai'
 
 import { GDF } from '../gdf.js'
+import { execSync } from 'node:child_process'
 
-fs.readdirSync('test', { withFileTypes: true }).forEach(entry => {
-  if (!entry.isDirectory()) return
-  if (!fs.existsSync(path.join('test', entry.name, 'package.json'))) return
+const entries = fs.readdirSync('test', { withFileTypes: true })
 
-  describe(entry.name, function () {
-    const workdir = path.join(os.tmpdir(), entry.name)
+describe('dockerfile-node-test', function () {
+  for (const entry of entries) {
+    if (!entry.isDirectory()) continue
+    if (!fs.existsSync(path.join('test', entry.name, 'package.json'))) continue
 
-    if (fs.existsSync(workdir)) fs.rmSync(workdir, { recursive: true })
+    describe(entry.name, function () {
+      const workdir = path.join(os.tmpdir(), entry.name)
 
-    fs.cpSync(path.join('test', entry.name), workdir, { recursive: true })
+      if (fs.existsSync(workdir)) fs.rmSync(workdir, { recursive: true })
 
-    it('should produce a dockerfile', async function () {
-      await new GDF().run(workdir)
+      fs.cpSync(path.join('test', entry.name), workdir, { recursive: true })
 
-      const actualResults = fs.readFileSync(path.join(workdir, 'Dockerfile'), 'utf-8')
-        .replaceAll(/^(ARG\s+\w+\s*=).*?(\s*\\?)$/gm, '$1xxx$2')
-
-      if (process.env.TEST_CAPTURE) {
-        fs.writeFileSync(path.join('test', entry.name, 'Dockerfile'), actualResults)
-      }
-
-      const expectedResults = fs.readFileSync(path.join('test', entry.name, 'Dockerfile'), 'utf-8')
-        .replaceAll(/^(ARG\s+\w+\s*=).*?(\s*\\?)$/gm, '$1xxx$2')
-
-      expect(expectedResults).to.equal(actualResults)
-    })
-
-    const pj = fs.readFileSync(path.join('test', entry.name, 'package.json'), 'utf-8')
-
-    it('should produce a .dockerignore', async function () {
-      await new GDF().run(workdir)
-
-      const actualResults = fs.readFileSync(path.join(workdir, '.dockerignore'), 'utf-8')
-
-      if (process.env.TEST_CAPTURE) {
-        fs.writeFileSync(path.join('test', entry.name, '.dockerignore'), actualResults)
-      }
-
-      const expectedResults = fs.readFileSync(path.join('test', entry.name, '.dockerignore'), 'utf-8')
-
-      expect(expectedResults).to.equal(actualResults)
-    })
-
-    if (pj.includes('prisma')) {
-      it('should produce a docker-entrypoint', async function () {
+      it('should produce a dockerfile', async function () {
         await new GDF().run(workdir)
 
-        const actualResults = fs.readFileSync(path.join(workdir, 'docker-entrypoint'), 'utf-8')
+        const actualResults = fs.readFileSync(path.join(workdir, 'Dockerfile'), 'utf-8')
+          .replaceAll(/^(ARG\s+\w+\s*=).*?(\s*\\?)$/gm, '$1xxx$2')
 
         if (process.env.TEST_CAPTURE) {
-          fs.writeFileSync(path.join('test', entry.name, 'docker-entrypoint'), actualResults)
+          fs.writeFileSync(path.join('test', entry.name, 'Dockerfile'), actualResults)
         }
 
-        const expectedResults = fs.readFileSync(path.join('test', entry.name, 'docker-entrypoint'), 'utf-8')
+        const expectedResults = fs.readFileSync(path.join('test', entry.name, 'Dockerfile'), 'utf-8')
+          .replaceAll(/^(ARG\s+\w+\s*=).*?(\s*\\?)$/gm, '$1xxx$2')
 
         expect(expectedResults).to.equal(actualResults)
       })
-    }
-  })
+
+      const pj = fs.readFileSync(path.join('test', entry.name, 'package.json'), 'utf-8')
+
+      it('should produce a .dockerignore', async function () {
+        await new GDF().run(workdir)
+
+        const actualResults = fs.readFileSync(path.join(workdir, '.dockerignore'), 'utf-8')
+
+        if (process.env.TEST_CAPTURE) {
+          fs.writeFileSync(path.join('test', entry.name, '.dockerignore'), actualResults)
+        }
+
+        const expectedResults = fs.readFileSync(path.join('test', entry.name, '.dockerignore'), 'utf-8')
+
+        expect(expectedResults).to.equal(actualResults)
+      })
+
+      if (process.env.DOCKER_BUILD) {
+        it('should build docker image successfully', async function () {
+          const dockerImageName = `dockerfile-node-test-${entry.name}`
+          await new GDF().run(workdir)
+
+          // build the docker image
+          try {
+            const results = execSync(`docker buildx build -t ${dockerImageName} .`, { cwd: workdir })
+
+            expect(results.toString()).to.not.match(/\bError:.*\b/)
+          } catch (err) {
+            expect('the test to run without an exception').to.equal('but it did not') // force test to fail, i don't know a better way to do this
+          }
+        })
+      }
+
+      if (pj.includes('prisma')) {
+        it('should produce a docker-entrypoint', async function () {
+          await new GDF().run(workdir)
+
+          const actualResults = fs.readFileSync(path.join(workdir, 'docker-entrypoint'), 'utf-8')
+
+          if (process.env.TEST_CAPTURE) {
+            fs.writeFileSync(path.join('test', entry.name, 'docker-entrypoint'), actualResults)
+          }
+
+          const expectedResults = fs.readFileSync(path.join('test', entry.name, 'docker-entrypoint'), 'utf-8')
+
+          expect(expectedResults).to.equal(actualResults)
+        })
+      }
+    })
+  }
 })
