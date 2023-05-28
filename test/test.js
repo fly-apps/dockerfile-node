5d4b94afa86ee3411b7f492bcaaaff160883dafc
Sam Ruby <rubys@intertwingly.net>
2023-05-28 19:50:34 -0400

    reorganize tests

--- test/test.js
+++ test/test.js
@@ -7,67 +7,69 @@ import { expect } from 'chai'
 import { GDF } from '../gdf.js'
 import { defaults } from '../index.js'
 
-const entries = fs.readdirSync('test', { withFileTypes: true })
+for (const group of fs.readdirSync('test', { withFileTypes: true })) {
+  if (!group.isDirectory()) continue
 
-for (const entry of entries) {
-  if (!entry.isDirectory()) continue
-  if (!fs.existsSync(path.join('test', entry.name, 'package.json'))) continue
+  for (const entry of fs.readdirSync(path.join('test', group.name), { withFileTypes: true })) {
+    if (!fs.existsSync(path.join('test', group.name, entry.name, 'package.json'))) continue
 
-  describe(entry.name, function() {
-    const workdir = path.join(os.tmpdir(), entry.name)
+    describe(entry.name, function () {
+      const workdir = path.join(os.tmpdir(), group.name, entry.name)
+      const testdir = path.join('test', group.name, entry.name)
 
-    if (fs.existsSync(workdir)) fs.rmSync(workdir, { recursive: true })
+      if (fs.existsSync(workdir)) fs.rmSync(workdir, { recursive: true })
 
-    fs.cpSync(path.join('test', entry.name), workdir, { recursive: true })
+      fs.cpSync(testdir, workdir, { recursive: true })
 
-    const pj = fs.readFileSync(path.join('test', entry.name, 'package.json'), 'utf-8')
-    const options = JSON.parse(pj).dockerfile || {}
-    options.force = true
+      const pj = fs.readFileSync(path.join(testdir, 'package.json'), 'utf-8')
+      const options = JSON.parse(pj).dockerfile || {}
+      options.force = true
 
-    it('should produce a dockerfile', async function() {
-      await new GDF().run(workdir, { ...defaults, ...options })
+      it('should produce a dockerfile', async function () {
+        await new GDF().run(workdir, { ...defaults, ...options })
 
-      const actualResults = fs.readFileSync(path.join(workdir, 'Dockerfile'), 'utf-8')
-        .replaceAll(/^(ARG\s+\w+\s*=).*?(\s*\\?)$/gm, '$1xxx$2')
-
-      if (process.env.TEST_CAPTURE) {
-        fs.writeFileSync(path.join('test', entry.name, 'Dockerfile'), actualResults)
-      }
-
-      const expectedResults = fs.readFileSync(path.join('test', entry.name, 'Dockerfile'), 'utf-8')
-        .replaceAll(/^(ARG\s+\w+\s*=).*?(\s*\\?)$/gm, '$1xxx$2')
-
-      expect(expectedResults).to.equal(actualResults)
-    })
-
-    it('should produce a .dockerignore', async function() {
-      await new GDF().run(workdir, options)
-
-      const actualResults = fs.readFileSync(path.join(workdir, '.dockerignore'), 'utf-8')
-
-      if (process.env.TEST_CAPTURE) {
-        fs.writeFileSync(path.join('test', entry.name, '.dockerignore'), actualResults)
-      }
-
-      const expectedResults = fs.readFileSync(path.join('test', entry.name, '.dockerignore'), 'utf-8')
-
-      expect(expectedResults).to.equal(actualResults)
-    })
-
-    if (pj.includes('prisma')) {
-      it('should produce a docker-entrypoint', async function() {
-        await new GDF().run(workdir, options)
-
-        const actualResults = fs.readFileSync(path.join(workdir, 'docker-entrypoint'), 'utf-8')
+        const actualResults = fs.readFileSync(path.join(workdir, 'Dockerfile'), 'utf-8')
+          .replaceAll(/^(ARG\s+\w+\s*=).*?(\s*\\?)$/gm, '$1xxx$2')
 
         if (process.env.TEST_CAPTURE) {
-          fs.writeFileSync(path.join('test', entry.name, 'docker-entrypoint'), actualResults)
+          fs.writeFileSync(path.join(testdir, 'Dockerfile'), actualResults)
         }
 
-        const expectedResults = fs.readFileSync(path.join('test', entry.name, 'docker-entrypoint'), 'utf-8')
+        const expectedResults = fs.readFileSync(path.join(testdir, 'Dockerfile'), 'utf-8')
+          .replaceAll(/^(ARG\s+\w+\s*=).*?(\s*\\?)$/gm, '$1xxx$2')
 
         expect(expectedResults).to.equal(actualResults)
       })
-    }
-  })
+
+      it('should produce a .dockerignore', async function () {
+        await new GDF().run(workdir, options)
+
+        const actualResults = fs.readFileSync(path.join(workdir, '.dockerignore'), 'utf-8')
+
+        if (process.env.TEST_CAPTURE) {
+          fs.writeFileSync(path.join(testdir, '.dockerignore'), actualResults)
+        }
+
+        const expectedResults = fs.readFileSync(path.join(testdir, '.dockerignore'), 'utf-8')
+
+        expect(expectedResults).to.equal(actualResults)
+      })
+
+      if (pj.includes('prisma')) {
+        it('should produce a docker-entrypoint', async function () {
+          await new GDF().run(workdir, options)
+
+          const actualResults = fs.readFileSync(path.join(workdir, 'docker-entrypoint'), 'utf-8')
+
+          if (process.env.TEST_CAPTURE) {
+            fs.writeFileSync(path.join(testdir, 'docker-entrypoint'), actualResults)
+          }
+
+          const expectedResults = fs.readFileSync(path.join(testdir, 'docker-entrypoint'), 'utf-8')
+
+          expect(expectedResults).to.equal(actualResults)
+        })
+      }
+    })
+  }
 }
